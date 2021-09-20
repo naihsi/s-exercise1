3954cb30cf5aaba726586dd96d5bad9615ae26ac
Naihsi Wang (王乃昕, ナイシ) <naihsi@gmail.com>
2021-09-20 19:50:00 +0000

    apply the pygithub to query if the mapping branch is idling in developing

---
+++ ngithub.py
@@ -0,0 +1,49 @@
+from github import Github
+from datetime import datetime
+
+
+def fetch_branches(repo_name, token):
+    g = Github(token)
+    repo = g.get_repo(repo_name)
+    
+    _branches = {}
+    for row in repo.get_branches():
+        _branches[row.name] = row.commit.sha
+    
+    return _branches
+
+
+#def get_commit_datetime(sha, repo_name, token):
+def commit_older_than_datetime(sha, datetime_now, shift, repo_name, token):
+    g = Github(token)
+    repo = g.get_repo(repo_name)
+    commit = repo.get_commit(sha=sha)
+    _commit_date = commit.commit.committer.date
+    _diff = int((datetime_now - _commit_date).total_seconds())
+    _mark = "<="
+    if _diff > shift:
+        _mark = ">"
+    print("{} - {} = {} {} {}".format(datetime_now, _commit_date, _diff, _mark, shift))
+    if _diff > shift:
+        return True
+    else:
+        return False
+
+
+##### ##### ##### #####
+# for testing
+
+def test():
+    _datetime_now = datetime.utcnow()
+    _repo_name = "naihsi/s-exercise1"
+    _token = "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"
+    _branches = fetch_branches(_repo_name, _token)
+    print(_branches)
+    
+    for k,v in _branches.items():
+        if commit_older_than_datetime(v, _datetime_now, 100, _repo_name, _token):
+            print("{} is too old".format(k))
+
+
+if __name__ == "__main__":
+    test()
--- recycle_instances.py
+++ recycle_instances.py
@@ -1,10 +1,16 @@
 import logging
 import sys
+from datetime import datetime
+
 import nboto3
+import ngithub
 
 
-def fetch_branches(datetime_now):
-    return {}
+def fetch_branches(repo_name, token):
+    return ngithub.fetch_branches(repo_name, token)
+
+def commit_older_than_datetime(sha, datetime_now, shift, repo_name, token):
+    return ngithub.commit_older_than_datetime(sha, datetime_now, shift, repo_name, token)
 
 
 def fetch_instances():
@@ -17,17 +23,24 @@ def terminate_instances(instance_ids):
 
 def main():
     # generate datetime
-    _datetime_now = ""
-
+    _datetime_now = datetime.utcnow()
+    _shift = 100
+    
     # fetch the branches
-    _branches = fetch_branches(_datetime_now)
+    _repo_name = "naihsi/s-exercise1"
+    _token = "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"
+    _branches = fetch_branches(_repo_name, _token)
     logger.debug(_branches)
 
     # check if each instance hits the threshold to recycle
     _to_recycle = []
     for row in fetch_instances():
-        # add to recycle list if the commit is too old
-        _to_recycle.append(row["instance_id"])
+        _branch_name = row["branch_name"]
+        _sha = _branches[_branch_name]
+        
+        if commit_older_than_datetime(_sha, _datetime_now, _shift, _repo_name, _token ):
+          # add to recycle list if the commit is too old
+          _to_recycle.append(row["instance_id"])
     logger.debug("to recycle: {}".format(_to_recycle))
 
     # terminate the candidates ids
