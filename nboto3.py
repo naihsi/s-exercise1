64f16ac57024a3c9c0d11542525a6c7c7accc591
Naihsi Wang (王乃昕, ナイシ) <naihsi@gmail.com>
2021-09-21 06:54:38 +0000

    add create an instance function and refactoring the wait for state

--- nboto3.py
+++ nboto3.py
@@ -44,27 +44,65 @@ def terminate_instances(instance_ids):
 
 
 def wait_for_terminated(instance_ids):
+    wait_for_state(instance_ids, "terminated")
+
+
+def wait_for_running(instance_ids):
+    wait_for_state(instance_ids, "running")
+
+
+def wait_for_state(instance_ids, wanted_state):
     ec2 = boto3.resource('ec2')
     _filters = [
-      {'Name': 'instance-state-name', 'Values': ['terminated']},
+      {'Name': 'instance-state-name', 'Values': [wanted_state]},
       {'Name': 'tag:branch_name', 'Values': ['dev_*']},
     ]
 
     # checking until all instances are terminated
     _ids = instance_ids.copy()
     while len(_ids) != 0:
-        print("checking termination: {}".format(_ids))
+        time.sleep(5)
+        print("checking state for {}: {}".format(wanted_state, _ids))
         for row in ec2.instances.filter(Filters=_filters):
             if row.instance_id in _ids:
-                print("{} terminated".format(row.instance_id))
+                print("{} {}".format(row.instance_id, wanted_state))
                 _ids.remove(row.instance_id)
-        time.sleep(2)
+
+
+def create_instance(branch_name):
+    _ami_image_id = "ami-087c17d1fe0178315"
+    _instance_type = "t2.micro"
+    _tags = [
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': "env: {}".format(branch_name)
+                },
+                {
+                    'Key': 'branch_name',
+                    'Value': branch_name
+                },
+            ]
+        },
+    ],
+    _client = boto3.client('ec2')
+    _response = _client.run_instances(ImageId=_ami_image_id, MinCount=1, MaxCount=1,
+                                      InstanceType=_instance_type,
+                                      TagSpecifications=_tags[0])
+    _instance_id = _response["Instances"][0]["InstanceId"]
+    print("the instace to launch for {}: {}".format(branch_name, _instance_id))
+    wait_for_running([_instance_id])
 
 
 ##### ##### ##### #####
 # for testing
 
 def test():
+    _branch_name = "dev_1"
+    create_instance(_branch_name)
+    return 0
     terminate_instances(['i-0681deb87361053dc', 'i-0d61305a8adc0593c'])
     return 0
     for row in fetch_instances():
